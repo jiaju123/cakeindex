43f3d1acfa030e322a32a2d8052a47f0697eb0fc
MalikCheng <296062888@qq.com>
2018-05-24 16:48:30 +0800

    das

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import VueResource from 'vue-resource';
+import './assets/css/base.css'
 
-Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.use(VueResource);
+
+Vue.http.options.emulateJSON = true;
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
