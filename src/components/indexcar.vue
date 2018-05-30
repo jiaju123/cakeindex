01ff792a72203ad2a77e1c170a5c6e1de6484b65
zhaoxin111111 <554777530@qq.com>
2018-05-30 22:42:50 +0800

    44444

--- src/components/indexcar.vue
+++ src/components/indexcar.vue
@@ -36,7 +36,7 @@
           <li class="tit4">操作</li>
         </ul>
         <ul class="cont">
-          <li v-for="">
+          <li v-for="item in drr">
             <div class="cont1">
 
               <div class="cont_img">
@@ -44,18 +44,19 @@
               </div>
 
               <div class="cont_text">
-                <p class="p1">奶油慕斯蛋糕 是讨好味觉的 艺术【草莓口味】</p>
-                <p class="p2">三种口味</p>
+                <!---->
+                <p class="p1">{{item.name}}</p>
+                <p class="p2">{{item.wei}}</p>
               </div>
             </div>
-            <div class="cont2">299</div>
-            <div class="cont2">2</div>
-            <div class="cont2">578</div>
+            <div class="cont2">{{item.price}}</div>
+            <div class="cont2">{{item.count}}</div>
+            <div class="cont2">{{item.total}}</div>
             <div class="cont3">
               <div class="cont3_a">
                 <img src="../assets/img3/qq .png"/>
               </div>
-              <p class="p4">运送中</p>
+              <p class="p4">{{item.status}}</p>
             </div>
             <div class="cont4">
               <div class="cont4_a">
@@ -65,64 +66,8 @@
               </div>
             </div>
           </li>
-          <li v-for="">
-            <div class="cont1">
 
-              <div class="cont_img">
-                <img src="../assets/img3/kk.png"/>
-              </div>
 
-              <div class="cont_text">
-                <p class="p1">奶油慕斯蛋糕 是讨好味觉的 艺术【草莓口味】</p>
-                <p class="p2">三种口味</p>
-              </div>
-            </div>
-            <div class="cont2">299</div>
-            <div class="cont2">2</div>
-            <div class="cont2">578</div>
-            <div class="cont3">
-              <div class="cont3_a">
-                <img src="../assets/img3/qq .png"/>
-              </div>
-              <p class="p4">运送中</p>
-            </div>
-            <div class="cont4">
-              <div class="cont4_a">
-
-                <button class="input"></button>
-                <img src="../assets/img3/tt.png" height="51" width="120"/>
-              </div>
-            </div>
-          </li>
-          <li v-for="">
-            <div class="cont1">
-
-              <div class="cont_img">
-                <img src="../assets/img3/kk.png"/>
-              </div>
-
-              <div class="cont_text">
-                <p class="p1">奶油慕斯蛋糕 是讨好味觉的 艺术【草莓口味】</p>
-                <p class="p2">三种口味</p>
-              </div>
-            </div>
-            <div class="cont2">299</div>
-            <div class="cont2">2</div>
-            <div class="cont2">578</div>
-            <div class="cont3">
-              <div class="cont3_a">
-                <img src="../assets/img3/qq .png"/>
-              </div>
-              <p class="p4">运送中</p>
-            </div>
-            <div class="cont4">
-              <div class="cont4_a">
-
-                <button class="input"></button>
-                <img src="../assets/img3/tt.png" height="51" width="120"/>
-              </div>
-            </div>
-          </li>
         </ul>
       </div>
     </div>
@@ -146,16 +91,21 @@
           {name:'我丨的丨订丨单',to:'/indexcar',id:''},
           {name:'我丨的丨购丨物丨车',to:'/incar',id:''},
         ],
-        drr:[],
-        created(){
-          this.$http.get("/api/index/orders").then(res=>{
-            console.log(res);
-            this.tableData=res.body;
-          })
-        },
+        drr:[
+
+        ],
+
       }
 
-    }
+    },
+    created(){
+      this.$http.get("/api/index/indexcar").then(res=>{
+        console.log(res);
+        this.drr=res.data;
+        console.log(this.drr);
+//            this.tableData=res.body;
+      })
+    },
 
   }
 </script>
